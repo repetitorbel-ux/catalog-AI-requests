f73798798722371f1f92ea1826759bc87dccf708
unknown <s:\!!HomeWork!!\The Bat!>
2025-08-29 12:25:18 +0300

    Задача 2.1: Переведена конфигурация на .env-файл

--- config.py
+++ config.py
@@ -1,14 +1,20 @@
 import os
+from dotenv import load_dotenv
+
+# Загружаем переменные окружения из .env файла
+load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Ключ для подписи сессий (необходим для flash-сообщений)
-SECRET_KEY = 'dev-secret-key-for-flask-flashing' # Для реального приложения стоит сгенерировать случайную строку
+# Берется из .env файла, с запасным значением для разработки
+SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key-for-flask-flashing')
 
 # --- Основная конфигурация базы данных ---
 
 # --- Удаленная БД (Neon) ---
-SQLALCHEMY_DATABASE_URI = 'postgresql://neondb_owner:npg_T9OUgFcmw7Ct@ep-tiny-leaf-afbopbk9-pooler.c-2.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+# Берется из .env файла, с запасным значением для разработки
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://neondb_owner:npg_T9OUgFcmw7Ct@ep-tiny-leaf-afbopbk9-pooler.c-2.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require')
 
 # --- Локальная тестовая БД (для разработки и проверки бэкапов) ---
 # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345678@localhost:5432/catalog_local'
